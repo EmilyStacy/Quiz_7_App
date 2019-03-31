fd0dfdfaca992fb9d7046569b28859743ce9eb25
Emily Stacy <beyonce@thinkful.com>
2019-03-31 15:47:02 -0500

    datastore.js

---
+++ datastore.js
@@ -0,0 +1,112 @@
+const STORE = [
+    {
+      number: 1,
+      question: 'In the TV show “fresh off the boat” which character is actually the writer of the original novel?',
+      answers: [
+        'Eddie Huang',
+        'Emery Huang',
+        'Evan Huang',
+        'Louis Huang'
+        ],
+      correctAnswer: 'Eddie Huang',
+    },
+    {
+      number: 2,
+      question: 'In “fresh off the boat” what was Grandma Huang’s English name?',
+      answers: [
+        'Jenna',
+        'Jessica',
+        'Jenny',
+        'Jen'
+        ],
+        correctAnswer: 'Jenny',
+    },
+    {
+      number: 3,
+      question: 'In season 1 where did Huang family move from? ',
+      answers: [
+        'Orlando, FL',
+        'Hong Kong',
+        'Washington D.C',
+        'Taiwan'
+        ],
+        correctAnswer: 'Washington D.C',
+    },
+    {
+      number: 4,
+      question: 'In Season 1 what exam did Jessica passed?',
+      answers: [
+        'Realtor',
+        'Insurance',
+        'Certified Public Accountant (CPA)',
+        'Nursing test'
+        ],
+      correctAnswer: 'Realtor',
+    },
+    {
+      number: 5,
+      question: 'In Season 2 whose concert was held in Orlando on Valentine’s Day?',
+      answers: [
+        'Jay Z',
+        'Janet Jackson',
+        'Dr.Dre',
+        'Michael Jackson'
+        ],
+      correctAnswer: 'Janet Jackson',
+    },
+    {
+      number: 6,
+      question: 'In Season 2 what was Jessica’s favorite TV show?',
+      answers: [
+        'Beverly Hills',
+        'Baywatch',
+        'Melrose Place',
+        'Pacific Palisades'
+        ],
+      correctAnswer: 'Melrose Place',
+    },
+    {
+     number: 7,
+      question: 'In Season 3 when Huang family flew to Taiwan,Jessica bought a pair of knock off shoes in the night market. What brand was it?',
+      answers: [
+      'Adidas',
+      'Nike',
+      'Gucci',
+      'Converse'
+      ],
+      correctAnswer: 'Nike',
+    },
+    {
+      number: 8,
+      question: 'In Season 3 which movie did Louis and Gary go to watch together? ',
+      answers: [
+        'Hamlet',
+        '10 things I hate about you',
+        'Shakespeare in Love',
+        'Romeo + Juliet'
+      ],
+      correctAnswer: 'Romeo + Juliet',
+    },
+    {
+     number: 9,
+      question: 'In Season 4 which celebrity’s death made Honey extremely upset?',
+      answers: [
+      'Mother Teresa',
+      'Princess Diana',
+      'Amy Whitehouse',
+      'Michael Jackson'
+      ],
+      correctAnswer: 'Princess Diana',
+    },
+    {
+      number: 10,
+      question: 'In Season 4 which celebrity was used as an example of hardworking by Jessica?',
+      answers: [
+        'Lucy Liu',
+        'Michelle Kwan',
+        'Tiger Woods',
+        'Jackie Chan'
+    ],
+    correctAnswer: 'Michelle Kwan',
+    }
+];
---
+++ index.js
@@ -0,0 +1,162 @@
+
+// set up the question number 
+let questionNumber = 0;
+//set up the score
+let questionScore = 0;
+//render questions template 
+function startTheQuiz(){
+  $('#js-button').on('click',function (event){
+    event.preventDefault();
+   $('#h1Title').hide();
+   $('#h2Title').hide();
+   $('#js-button').hide();
+   $("#newQuestion").text(1);
+   generateNewQuestion();
+  });
+};
+//CSS formating does not look right. Keep working later;pull in questions
+//new question format
+function generateNewQuestion(){
+  $("#question_container").html(
+      `<section id = "questionPage">
+      <div>
+        <h2 class="header2"> ${STORE[questionNumber].question} </h2>
+      </div>
+        <div>
+          <form id="QuestionChoices">
+            <fieldset class = "questionFormat">
+                        <span class= "formSpan"><input type="radio"  value="${STORE[questionNumber].answers[0]}" name="Choice" checked> ${STORE[questionNumber].answers[0]} </input></span>
+                            <span class= "formSpan"><input type="radio"   value="${STORE[questionNumber].answers[1]}" name="Choice"> ${STORE[questionNumber].answers[1]} </input></span>
+            <span class= "formSpan"><input type="radio"  value="${STORE[questionNumber].answers[2]}"name="Choice"> ${STORE[questionNumber].answers[2]} </input></span>
+           <span class= "formSpan"><input type="radio"  value="${STORE[questionNumber].answers[3]}" name="Choice"> ${STORE[questionNumber].answers[3]} </input></span>
+           </fieldset>
+          </form>
+          <div class="button">
+            <button type="button" id="js_submit"> Submit </button>
+          </div>
+        </div> 
+       </section>`
+  );
+       console.log('generateNewQuestion ran');
+};
+
+//what happened after hitting submit
+function submitQuestion(){
+  $('#question_container').on('click', '#js_submit',function(event){
+    event.preventDefault();
+    $('#questionPage').hide();
+    feedbackGenerate();
+  });
+}
+//Create a feedback
+function feedbackGenerate(questionAnswer, questionInput){ 
+   questionAnswer = STORE[questionNumber].correctAnswer;
+   questionInput = $("input:checked").val();
+   
+    if (questionAnswer === questionInput){
+      correctFeedback();
+    } else 
+    {
+      incorrectFeedback();
+    }
+  console.log(`feedbackGenerate ran`);
+  //switchQuestion();<redundant>
+};
+//what happened after clicking "next"
+function switchQuestion(){
+  $('#question_container').on('click','#js_next',function (event){
+    event.preventDefault();
+      $('#questionPage').hide();
+    let i = questionNumber;
+    if (i === STORE.length -1){
+     finalResult();
+     $("#newQuestion").text(10);
+
+    } else if (i < STORE.length){
+      changeQuestionNum();
+      generateNewQuestion();
+      i++;
+    }
+   
+  }
+  )
+  console.log('switchQuestion ran');
+};
+
+//correct feedback
+function correctFeedback(){
+  let questionAnswer = STORE[questionNumber].correctAnswer;
+$('#question_container').html(`<div class="questionFormat" id="feedBack"> <span class ="header2"> CORRECT! The answer is <span class="spanquestionAnswer"> ${questionAnswer} </span></span> </div>
+      <div class="button">
+        <button type="button" id="js_next"> Next </button>
+       </div>`);
+  changeScore();
+};
+
+//incorrect feedback
+function incorrectFeedback(){
+  const questionAnswer = STORE[questionNumber].correctAnswer;
+  $('#question_container').html(`<div class="questionFormat"> <span class ="header2" id="feedBack"> SORRY! The answer is <span class="spanquestionAnswer">${questionAnswer}</span>. Try again later. </span> </div>
+      <div class="button"><button type="button" id="js_next"> Next </button>
+      </div>`);
+};
+
+//change the score after "Next"
+function changeScore(){
+questionScore++;
+$("#newScore").text(questionScore);
+};
+
+//change question number
+function changeQuestionNum() {
+  questionNumber++;
+  $("#newQuestion").text(questionNumber + 1);
+   console.log(questionNumber);
+};
+//generate the final result
+function finalResult(){
+  //$('#question_container').hide("#questionPage");
+  //$('#question_container').hide("#resultFooter");
+ if(`${questionScore}` < 6) {
+$('#question_container').html(`<div class="questionFormat"> <span class ="formSpan"> You have finished the quiz. <p class="enlarge">Final score: <em>${questionScore}</em></p><div>If you want to try again, click the button to restart! </div></span> </div>
+      <div class="button"><button type="button" id="js_restart"> Restart </button>
+       </div>`
+  );
+  $("#newScore").css({
+ "color": "#f0ea94",
+ //"font-size":"2em",
+  });
+ } else {
+  $('#question_container').html(`<div class="questionFormat"> <span class ="formSpan"> Congratulations! You have finished the quiz. <p class="enlarge">Final score:  ${questionScore}.</p><div>If you want to try again, click the button to restart! </div></span> </div>
+      <div class="button"><button type="button" id="js_restart"> Restart </button>
+       </div>`
+  );
+  $("#newScore").css({
+ "color": "#f0ea94",
+  });
+ };
+  console.log('finalResult ran!');
+  reStart();
+};
+
+//
+//restart
+function reStart(){
+  $('#question_container').on('click','#js_restart',function(event){
+    event.preventDefault();
+    window.location.reload();
+    //$('body').html('#startPage');
+  });
+  console.log('reStart ran');
+};
+
+/*finalfunction*/
+
+function calltheApp(){
+  //renderQuestion();
+  startTheQuiz();
+  submitQuestion();
+  switchQuestion();
+}
+
+$(calltheApp);
